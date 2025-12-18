d5a457117ef5a36fe06c346e53782e71938d20a0
Pranay Gurav <guravpranay81@gmail.com>
2025-12-18 08:47:59 +0530

    Prepare backend for deployment

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ import activityRoutes from "./routes/activity.routes.js";
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://YOUR_FRONTEND_URL.vercel.app"
+    ],
+    credentials: true
+}));
+
 app.use(express.json())
 app.get("/", (req, res) => {
     res.send("APi is running");
